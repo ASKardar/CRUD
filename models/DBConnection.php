6d7f783939633dd07e003775270cca4029e4be2d
ASKardar <askardar@gmail.com>
2018-11-21 18:11:15 +0500

    Update DBConnection.php

--- models/DBConnection.php
+++ models/DBConnection.php
@@ -4,7 +4,7 @@ class DBConnection
 {
     public function query($query)
     {
-        $conn = new mysqli("localhost", "root", "", "crud");
+        $conn = new mysqli("https://db4free.net:3306", "cruduser", "crudtest", "cruddatabase");
         if($conn->connect_errno)
             die($conn->connect_error);
         $result = $conn->query($query);
